ff2e52668e4ec3c75a091f62a516c6b0b9fa4707
Sonia-trisher <soniatrisher86@gmail.com>
2025-02-19 02:23:18 -0800

    yh

---
+++ script.js
@@ -0,0 +1,131 @@
+// QUESTION 2: Recursion and Algorithmic Thinking 
+function sumDigits(n){
+    let sum = 0;
+    for( let i=1;i<n.length;i++){
+        sum = sum + n.length
+    }
+    return sum;
+
+}
+// console.log(sumDigits([1,2,3]));
+function power(base,exponent){
+    
+    return base^exponent;
+
+}
+// console.log(power(3,2));
+function reverseWords(sentence){
+    
+    reverseWords.reverses()
+}
+// console.log(reverseWords("Hello World"));
+// others on this question challenging me
+
+
+
+// QUESTION 4: Implementing a Simple Voting System for Bootcamp Challenges 
+function countProjectVotes(votes) {
+    if(votes=2){
+        console.log("project A")
+        
+    }else{
+        console.log("project B")
+    
+    }
+ return countProjectVotes;
+}
+const votes = [ 
+  { student: "Alice", project: "Project A" }, 
+  { student: "Bob", project: "Project B" }, 
+ 
+  { student: "Charlie", project: "Project A" } 
+]; 
+// b
+// function findWinningProject(votes) {
+return Math.max(votes);
+// }
+// console.log(findWinningProject(2,1));
+// c
+function getUniqueProjects(votes) {
+    const student = votes.student;
+
+    return votes.filter(student === "Bob")
+}
+// console.log(getUniqueProjects("Charlie","Bob","Alice"));
+// d
+const findWinningProject(votes)=[];
+console.log(findWinningProject());
+
+
+
+
+
+
+ 
+// QUESTION 3: Banking Transaction System
+function calculateBalance(transactions, accountHolder) {
+    if(users==! transaction){
+        console.log("No transaction")
+        
+    }else{
+        console.log([withDraw,deposit]);
+        
+    }
+
+
+}
+// const transaction=['deposit','withDraw'];
+const users = [
+    {
+        name:"keza",
+        transation:"deposit",
+    },
+    {
+        name:"sonia",
+        transation:"WithDraw",
+    },
+    {
+        name:"kandi",
+        transation:"",
+    },
+    {
+        name:"dada",
+        transation:"",
+    },
+];
+// console.log(calculateBalance(users));
+
+ 
+// QUESTION 1: Functions, Loops, and Conditionals
+// a
+function countMembershipLevels(members){
+    return members;
+
+}
+let members = [
+    {
+        name:"ateta",
+        email:"ttt@gmail.com",
+         silver: 2
+    },
+    {
+        name:"uwamahoro",
+        email:"sss@gmail.com",
+        gold: 3,
+    },
+    {
+        name:"delse",
+        email:"ddd@gmail.com",
+        bronze: 4, 
+    },
+];
+console.log(countMembershipLevels(members));
+// b
+function getGoldMembers(members) {
+    members.filter(members=>members.name)
+}
+
+
+
+
+
